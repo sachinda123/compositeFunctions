42c732bbc594f83be4344cc2cd868bc87c757a40
sachinda <sachinda@parkwaylabs.com>
2024-07-23 19:15:01 +0530

    last

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+const split = (str) => {
+  return str.split("");
+};
+const reverse = (strArray) => {
+  return strArray.reverse();
+};
+const join = (strArray) => {
+  return strArray.join("");
+};
+
+const compose = (str) => join(reverse(split(str)));
+
+console.log("fff", compose("hellow"));
+
 app.listen(3000, () => {
   console.log("app started");
 });
